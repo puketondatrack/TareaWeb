bf5ed1988a1bc935bf58bc4f5e5e05f9d467f2f3
Kevin Rivera <kevin14109836@gmail.com>
2022-05-31 21:19:12 -0600

    Tarea push 1

---
+++ src/main/java/tareaWeb/tareaWeb/TareaWebApplication.java
@@ -0,0 +1,13 @@
+package tareaWeb.tareaWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TareaWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TareaWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tareaWeb/tareaWeb/TareaWebApplicationTests.java
@@ -0,0 +1,13 @@
+package tareaWeb.tareaWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TareaWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
